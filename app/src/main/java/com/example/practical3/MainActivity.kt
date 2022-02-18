a319c6353f687b4a7f0ec364dc4d6669e5f4c938
Zheng240901 <tongzh-wm19@student.tarc.edu.my>
2022-02-19 00:29:41 +0800

    First commit

---
+++ app/src/main/java/com/example/practical3/MainActivity.kt
@@ -0,0 +1,122 @@
+package com.example.practical3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.practical3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        reset()
+        binding.btnReset.setOnClickListener() { reset() }
+        binding.btnSubmit.setOnClickListener() { submit() }
+
+    }
+
+    private fun reset() {
+
+        binding.edtName.text.clear()
+        binding.rgpGender.check(R.id.radFemale)
+        binding.spnProgram.setSelection(0)
+        binding.edtYear.text.clear()
+        binding.chkSponsorship.isChecked = false
+
+        binding.edtName.requestFocus()
+    }
+
+    private fun submit() {
+        //get input
+        val name = binding.edtName.text.toString().trim()
+        val gender = getGender()
+        val program = binding.spnProgram.selectedItem as String
+        val sponsorship = binding.chkSponsorship.isChecked
+        val year = binding.edtYear.text.toString().toIntOrNull() ?: 0
+        //meaning of this line of code above ↑,
+        // if(binding.edtYear.text.toString().toIntOrNull() != NULL)
+        // return year;
+        // else
+        // return 0;
+
+
+        //calculation
+        val fee = getFee(program, year, sponsorship)
+
+        //validation
+        //name validation
+        if(name == "")
+        {
+            toast(getString(R.string.invalid_name))
+            return
+        }
+
+        //year validation
+        if(year < 1 || year > 3)
+        {
+            toast(getString(R.string.invalid_year))
+            return
+        }
+
+
+
+        //Explicit intent
+        val intent = Intent(this,SubActivity::class.java)
+            .putExtra("name",name)
+            .putExtra("gender",gender)
+            .putExtra("program",program)
+            .putExtra("year",year)
+            .putExtra("sponsorship",sponsorship)
+            .putExtra("fee",fee)
+
+        startActivity(intent)
+    }
+//----------------------------------------------------------------------------------------
+    //functions
+    private fun getFee(program: String, year: Int, sponsorship: Boolean): Double {
+        val base = when(program) {
+            "RIT" -> when (year) {
+                1 -> 11000.0
+                2 -> 12000.0
+                3 -> 13000.0
+                else -> 0.0
+            }
+            "RST" -> when (year) {
+                1 -> 14000.0
+                2 -> 15000.0
+                3 -> 16000.0
+                else -> 0.0
+            }
+            "RSF" -> when (year) {
+                1 -> 17000.0
+                2 -> 18000.0
+                3 -> 19000.0
+                else -> 0.0
+            }
+            else -> 0.0
+        }
+
+        val rate = if(sponsorship) 0.5 else 1.0 //if sponsorship checkbox is checked then give 50% off
+        return base * rate
+    }
+
+    private fun toast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    private fun getGender(): String {
+        //return buttonId when it is chosen return nothing if both not chosen
+        return when(binding.rgpGender.checkedRadioButtonId) {
+            R.id.radFemale -> getString(R.string.female)
+            R.id.radMale -> getString(R.string.male)
+            else -> ""
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/practical3/SubActivity.kt
@@ -0,0 +1,34 @@
+package com.example.practical3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.practical3.databinding.ActivitySubBinding
+
+class SubActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySubBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivitySubBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnClose.setOnClickListener(){ finish() }
+
+        //get results
+        val name = intent.getStringExtra("name") ?: ""
+        val gender = intent.getStringExtra("gender") ?: ""
+        val program = intent.getStringExtra("program") ?: ""
+        val year = intent.getIntExtra("year",0)
+        val sponsorship = intent.getBooleanExtra("sponsorship", false)
+        val fee = intent.getDoubleExtra("fee",0.0)
+
+        //print out results
+        binding.txtName.text = name
+        binding.txtGender.text = gender
+        binding.txtProgram.text = program
+        binding.txtYear.text = year.toString()
+        binding.txtSponsorship.text = if(sponsorship) getString(R.string.yes) else getString(R.string.no)
+        binding.txtFees.text = "RM %.2f".format(fee)
+    }
+}
